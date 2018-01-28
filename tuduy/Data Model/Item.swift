20946ec44e943665ffd4d24817594ab86b533d3f
alvin <alvin@alvins-imac>
2018-01-28 09:07:20 -0800

    convert to mvc

---
+++ tuduy/Data Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  tuduy
+//
+//  Created by alvin on 1/24/18.
+//  Copyright © 2018 alvin. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+}
+
+
